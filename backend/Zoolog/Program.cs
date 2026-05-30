28fa9fecc4d5ef86416be66d58a17e02bd8f1ce1
Lukas Jung <lukasojung@gmail.com>
2026-05-30 22:36:27 +0200

    started connecting frnt and back

--- backend/Zoolog/Program.cs
+++ backend/Zoolog/Program.cs
@@ -2,28 +2,45 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddOpenApi();
+
+var allowedOrigins = builder.Configuration.GetValue<string>("allowedOrigins")!.Split(",");
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins(allowedOrigins).AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    //app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
+
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
+
+app.UseCors("AllowAngular");
+
+//app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.MapControllers();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+//app.MapStaticAssets();
 
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}")
+//     .WithStaticAssets(); 
 
 app.Run();
