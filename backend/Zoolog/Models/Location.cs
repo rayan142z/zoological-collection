a32a0a4395ab00cf9fc7d76c1cd129c6c9174319
Alexander Stojek <alx.stojek@gmail.com>
2026-07-17 11:23:09 +0200

    fix: Make Country and Region properties nullable in Location model

--- backend/Zoolog/Models/Location.cs
+++ backend/Zoolog/Models/Location.cs
@@ -6,7 +6,7 @@ public class Location
     public string Name { get; set; }
     public decimal Latitude { get; set; }
     public decimal Longitude { get; set; }
-    public string Country { get; set; }
-    public string Region { get; set; }
+    public string? Country { get; set; }
+    public string? Region { get; set; }
 
 }
